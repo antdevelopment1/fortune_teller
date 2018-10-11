2079915c00d5cc53a1dac4a040034f9d47ab3d2e
April Copes <antdevelopment1@gmail.com>
2018-10-11 15:00:41 -0400

    Adds Photo Swap Click Event Listener

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,9 @@ console.log('Hello I am here');
 
 // 1. Make some code run when I click the button.
 const triggerElement = document.querySelector('[data-trigger]');
+const triggerElementHobbies = document.querySelector('[data-hobbies]');
 const outputElement = document.querySelector('[data-output]');
+const imageElement = document.querySelector('[data-image]');
 
 const fortunes = [
     "You laugh now, wait till you get home.",
@@ -14,22 +16,63 @@ const fortunes = [
     "About time I got out the cookie.",
     "The world is your oyster.",
     "Thank god it's Friday."
-]
+];
+
+const photos = [
+    {
+        text: "You will be ambushed by ninjas",
+        url: "/images/waterfall.jpg"
+    },
+    {
+        text: "You will be ambushed by popcorn",
+        url: "/images/mountains.jpg"
+    },
+    {
+        text: "You will be ambushed by hamburgers",
+        url: "/images/prettytree.jpg"
+    },
+    {
+        text: "You will be ambushed by paper towels",
+        url: "/images/pexels-photo-236047.jpeg"
+    },
+];
+
+let index = Math.floor((Math.random() * fortunes.length));
+let photosIndex = 0;
 
 // addEvenet Listeners that accepts two arguments
 // a string that indentifies the kind of event to listen for
 // a function that says what to do when the event happens
 
-
+// Pagnation
 triggerElement.addEventListener('click', function() {
 
   console.log('Hello event listener.')
 
-  const newFortune = fortunes.pop();
+  const newFortune = fortunes[index];
 
   outputElement.textContent = newFortune;
 
+  index = Math.floor((Math.random() * fortunes.length));
+
+  console.log(index);
+
 });
+triggerElementHobbies.addEventListener('click', function() {
+    console.log('Connected');
+    // photos.forEach(function() {
+
+        imageElement.src = photos[photosIndex].url;
+
+        console.log(photosIndex);
+
+        photosIndex++;
+
+        if (photosIndex === photos.length) {
+            photosIndex = 0;
+        } 
+   
+})
 // 2. Write some text to the page on buttton click.
 
 outputElement.addEventListener('click', function() {
